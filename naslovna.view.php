455f96e3e8f143e5943978fea4ccdf473712d18e
Marin Zilic <marinzilic507@gmail.com>
2022-06-30 21:19:27 +0200

    commit

--- naslovna.view.php
+++ naslovna.view.php
@@ -248,30 +248,7 @@
                 
     </div>
 
-    <div class="footer">
-                <div class="footer_details">
-                    <div class="social">
-                        <i class="fa fa-facebook-f" style="font-size:31px"></i>
-                        <i class="fa fa-twitter" style="font-size:31px"></i>
-                        <i class="fa fa-youtube-play" style="font-size:31px"></i>
-                    </div>
-
-                    <div class="text">
-                        <p>© 2022, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal <br> Tournament, and the Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other <br> brands or product names are the trademarks of their respective owners. Non-US transactions through Epic Games International, S.à r.l. </p>
-                    </div>
-
-                    <hr>
-
-                    <div class="prava">
-                        <p>Terms of Service</p>
-                        <p>Privacy Policy</p>
-                        <p>Store Refund Policy</p>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        
+    
 
         
       
